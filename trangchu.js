0fb362dce4da9a7bc79fa083b9ef3f0cc4ef490c
minhtan39 <161004292+minhtan39@users.noreply.github.com>
2024-08-09 06:26:46 +0700

    Update trangchu.js

--- trangchu.js
+++ trangchu.js
@@ -1,8 +1,17 @@
-function showSidebar(){
-    const sidebar = document.querySelector(`.sidebar`)
-    sidebar.style.display = `flex`
+function toggleSidebar() {
+    const sidebar = document.querySelector('.sidebar');
+    const menuButton = document.querySelector('.menu-button');
+    const isOpen = sidebar.classList.toggle('open');
+    menuButton.classList.toggle('open', isOpen);
 }
-function hideSidebar(){
-    const sidebar = document.querySelector(`.sidebar`)
-    sidebar.style.display = `none`
-}
+/*Chạm vào bất kỳ Sidebar thu vào*/
+document.addEventListener('click', (event) => {
+    const sidebar = document.querySelector('.sidebar');
+    const menuButton = document.querySelector('.menu-button');
+
+    // Kiểm tra nếu nhấp ra ngoài sidebar và menu button
+    if (!sidebar.contains(event.target) && !menuButton.contains(event.target)) {
+        sidebar.classList.remove('open');
+        menuButton.classList.remove('open');
+    }
+});
